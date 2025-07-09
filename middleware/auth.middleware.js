4548ca93da0ecb8b0c93fe13740c700894ddee51
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-09 19:50:09 +0530

    Add authentication middleware to verify JWT tokens

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+dotenv.config()
+export function authUser(req, res, next) {
+  const header = req.header("Authorization");
+  if (header) {
+    const token = header.replace("Bearer ", "");
+    jwt.verify(token,process.env.JWT_KEY, (err, decode) => {
+      console.log(decode);
+      if (decode !== null) {
+        req.user = decode;
+      
+      }
+    });
+  }
+  
+  next()
+}
