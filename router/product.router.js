ad7d6b7d18c5cb3924cb5f3601c0fa97183abab1
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-09 19:50:39 +0530

    Add product router with routes for creating, retrieving, updating, and deleting products

---
+++ router/product.router.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { createProduct,getAllProducts,searchProduct,updateProduct,deleteProduct } from "../controller/product.controller.js";
+
+const productRouter = express.Router();
+
+productRouter.post('/',createProduct);
+productRouter.get('/products',getAllProducts);
+productRouter.get('/:id',searchProduct);
+productRouter.put('/:id',updateProduct);
+productRouter.delete('/:id',deleteProduct);
+
+export default productRouter;
+
