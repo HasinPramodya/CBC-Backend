c66d9bc3800b83306135d364b20a8cf33d59a58e
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-19 17:29:43 +0530

    feature: add getProductById Api to product router

--- router/product.router.js
+++ router/product.router.js
@@ -1,5 +1,12 @@
 import express from "express"
-import { createProduct,getAllProducts,searchProduct,updateProduct,deleteProduct } from "../controller/product.controller.js";
+import {
+    createProduct,
+    getAllProducts,
+    searchProduct,
+    updateProduct,
+    deleteProduct,
+    getProductById
+} from "../controller/product.controller.js";
 
 const productRouter = express.Router();
 
@@ -8,6 +15,7 @@ productRouter.get('/products',getAllProducts);
 productRouter.get('/search/:id',searchProduct);
 productRouter.put('/:id',updateProduct);
 productRouter.delete('/:id',deleteProduct);
+productRouter.get('/:id',getProductById);
 
 export default productRouter;
 
